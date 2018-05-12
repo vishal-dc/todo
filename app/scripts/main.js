dc6ffc7a18c7a66241b09ff54f22424f8b66bb9d
vishal-dc <vishal.dc@gmail.com>
2018-05-12 17:21:21 +0530

    create user with local storage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,43 +12,71 @@ $(function(){
             console.error('error during sync', arguments);
         }
     }
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+      };
+
+    if(!localStorage.getItem('usersCounter'))
+        localStorage.setItem('usersCounter', 1);
+
+    var db = {};
+    db.users = [];    
+  
     function createLocalStorageSyncFn(key){
         return function syncTolocalStorage(method, model, options) {            
                 try{
 
                     switch(method) {
                         case 'read':
-                            var data = JSON.parse(localStorage.getItem(key));
-                            model.set(data);     
-                            options.success(data);               
+                            model = JSON.parse(localStorage.getItem(key));    
+                            db[key] = model;                        
                             break;
                         case 'update':
-                        case 'create':
+
                             localStorage.setItem(key, JSON.stringify(model.toJSON()));
+                            break;
+                        case 'create':
+                            var idCounter = Number(localStorage.getItem(key+'Counter'));
+                            model.set('id', idCounter);
+                            db[key].push(model.toJSON());
+                            // db[key][idCounter].id=idCounter;
+                            localStorage.setItem(key+'Counter', idCounter+1);                                                        
+                            localStorage.setItem(key, JSON.stringify(db[key]));                            
                             break;
                         case 'delete':
                             localStorage.setItem(key, null)
                         }
+                        options.success(model || {});               
    
                 }catch(e){
+                    console.error(e);
                     options.error();
-                    console.error('error during sync', arguments);
+                    console.error('error during sync', e, arguments);
                 }
             };
     }
 
-    
-
-
-
     var Users = Backbone.Collection.extend({
-        url: 'data/users.json',
+        url: 'data/users',
         sync: createLocalStorageSyncFn('users')
 
     });
 
+
     var UsersView = Backbone.View.extend({
-        el: $('#main-view'),
+        el: '#main-view',
         template: _.template($('#users-template').html()),
         // tagName: "li",
     
@@ -60,10 +88,7 @@ $(function(){
         //   "click .button.delete": "destroy"
         // },
     
-        // initialize: function() {
-        //   this.listenTo(this.model, "change", this.render);
-        // },
-    
+
         render: function() {
             var users = new Users();
             var that = this;
@@ -78,7 +103,40 @@ $(function(){
         }
     
     });
-    var UsersView = new UsersView();
+
+    
+    var User = Backbone.Model.extend({
+        url: 'data/users',
+        sync: createLocalStorageSyncFn('users')
+
+    });
+
+    var UserView = Backbone.View.extend({
+        el: '#main-view',
+        template: _.template($('#user-template').html()),
+        events: {
+          "submit .edit-user-form": "saveUser"
+        },
+    
+        render: function() {            
+            this.$el.html(this.template({user:null}));
+
+            return this;
+        },
+
+        saveUser: function(event){
+            var userDetails = $(event.currentTarget).serializeObject();
+            var user = new User();
+            user.save(userDetails, {
+                success: function(){
+                    router.navigate('#users', {trigger: true});
+                }
+            });
+            return false;
+        }
+    
+    });
+   
     var Router = Backbone.Router.extend({
 
         initialize: function () {
@@ -89,26 +147,27 @@ $(function(){
             //   });
         },
 
-        execute: function(callback, args, name) {                     
-            if (callback) {
-                //TODO use view.close/remove
-                console.log("Clear Main View");
-                $( "#main-view" ).empty();
-                callback.apply(this, args);
-            }else{
-                console.log("Something wrong!");
-                alert('Incorrect Route');
-                return false;
-            }
+        // execute: function(callback, args, name) {                     
+        //     if (callback) {
+        //   
+        //         console.log("Clear Main View");
+        //         $( "#main-view" ).empty();
+        //         callback.apply(this, args);
+        //     }else{
+        //         console.log("Something wrong!");
+        //         alert('Incorrect Route');
+        //         return false;
+        //     }
 
-        },
+        // },
 
         routes: {
             '': 'home',  
             'about': 'about',    
             'users': 'users', 
-            'users/:id': 'user',
-            'search/:query/p:page': 'search'  
+            'users/edit/:id': 'user',
+            'users/create': 'user'
+            
         },
 
         home: function(){
@@ -125,23 +184,23 @@ $(function(){
 
         users: function() {
             console.log('In users');
-
-            UsersView.render();        
-
-           
+            usersView.render();       
+          
         },
 
         user: function(id) {
             
             console.log('In user for id: ', id);
+            userView.render();
         }
 
     }); 
 
     
-          
+    var usersView = new UsersView();        
+    var userView = new UserView();        
     /** */
-    new Router();
+    var router = new Router();
     
     $.get( "/data/users.json")
         .done(function(data) {          
@@ -151,5 +210,6 @@ $(function(){
           alert( "error" );
         });
        
+
     
   });
